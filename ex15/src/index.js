222e25677355647dca2a1c182a5aa3cf4c6343b2
timurseta <timur_seta@hotmail.com>
2021-09-22 01:28:56 +0200

    do while and if inside

---
+++ ex15/src/index.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+function myDoWhile() {
+    var myNumbers = "";
+    var i = 0;
+    do {
+        if (i != 9) {
+            myNumbers += i + ",";
+            i++;
+        } else {
+            myNumbers += i;
+            i++;
+        }
+    } while (i < 10) 
+
+    return myNumbers;
+
+}
+console.log(myDoWhile());
+// Only change code above this line
+module.exports = myDoWhile;
